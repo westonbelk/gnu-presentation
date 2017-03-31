8b755a4ef400f1e5162ddd90b1d478f777d93357
Weston Belk <wbelk1289@gmail.com>
2017-03-31 01:15:36 -0400

    Added setup script and picture of stallman

---
+++ setup.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# Copyright (c) <2017> <Weston Belk>
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+# 
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+ 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+mkdir reveal.js/content
+cp slides.md reveal.js/content/slides.md
+cp images/stallman.jpg reveal.js/content/stallman.jpg
+cp images/coreutils.png reveal.js/content/coreutils.png
+
+# cd reveal.js
+# http-server
